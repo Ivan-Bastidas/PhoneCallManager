755313babe6fa19aa19fdabaa13f7477dd4d62cc
Ivan Bastidas <ivan.bastidas@admios-sa.com>
2015-02-06 12:45:49 -0500

    Adding comments

--- src/main/java/com/admios/utils/Helpers.java
+++ src/main/java/com/admios/utils/Helpers.java
@@ -8,10 +8,16 @@ import java.util.regex.Pattern;
  */
 public class Helpers {
 
+    /*
+    * Creates a single String with the numbers in the format from->to
+    */
     public static String stringifyCall(final String from, final String to) {
         return String.format("%s->%s", from, to);
     }
 
+    /*
+    * Validates that the number is in the correct format (ITU-T standards)
+    */
     public static boolean validateNumber(String number) {
         String regex = "^\\+(?:[0-9] ?){6,14}[0-9]$";
         Pattern pattern = Pattern.compile(regex);
